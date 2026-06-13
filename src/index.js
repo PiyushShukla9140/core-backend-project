e03a53cc3d2be8e9c53b54c867cbab1b747a1728
Piyush Shukla <piyushshukla8980@gmail.com>
2026-06-13 14:56:41 +0530

    Database connected successfully

---
+++ src/index.js
@@ -0,0 +1,38 @@
+// require(`dotenv`).config({path:"./env"})
+import mongoose from "mongoose"
+import connectDB from "./db/index.js"
+import express from "express"
+import dotenv from "dotenv"
+import {DB_NAME} from "./constants.js"
+
+
+connectDB()
+
+const app = express()
+
+dotenv.config({
+    path:"./env"
+})
+
+// function connectdb(){}
+
+// connnectdb() this can be also used
+// but we are going to use IIFE function
+;(async()=>{
+    try {
+        //console.log("DEBUG URI VALUE:", process.env.MONGODB_URI);
+        const connectdb = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        // isme listeners bhi use hote h
+        app.on("error",()=>{
+            console.log("Error connecting database")
+            throw error
+        })
+        
+        app.listen(process.env.PORT, ()=>{
+            console.log(`App is listening on port ${process.env.PORT}`)
+        })
+    } catch (error) {
+        console.error("Error connecting database",error)
+        throw error
+    }
+})()
