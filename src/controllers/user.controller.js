e3908a4c6bb1dfe5f9aa1c86d89aadcebd593bac
Piyush Shukla <piyushshukla8980@gmail.com>
2026-06-24 14:11:41 +0530

    completed channel profile controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -472,6 +472,83 @@ const updateUserCoverImage = asyncHandler(async(req,res)=>{
 
 })
 
+const getUserChannelProfile = asyncHandler(async(req,res)=>{
+    const {username} = req.params
+
+    if(!username?.trim()){
+        throw new ApiError(400, "Username is missing")
+    }
+
+    const channel = await User.aggregate([
+        {
+            $match:{
+                username:username?.toLowerCase()
+            }
+        },
+        {
+            $lookup:{
+                from="subscriptions",// humne model ka Subscription rkha tha lekin databse me naam change ho gya tha
+                localField:"_id",
+                foreignField:"channel",
+                as:"subscribers"
+            }
+        },
+        {
+           $lookup:{
+                from="subscriptions",// humne model ka Subscription rkha tha lekin databse me naam change ho gya tha
+                localField:"_id",
+                foreignField:"subscriber",
+                as:"subscribedTo"
+           } 
+        },
+        {
+            $addFields:{
+                subscribersCount: {
+                    $size: "$subscribers"
+                },
+                channelsSubscribedToCount: {
+                    $size: "$subscribedTo"
+                },
+                // what about the subscribe button, if user has already subscribed then it should not show the button
+                // frontend developer ko true yaa false message bhej dnege ke 
+                // true hai toh subscribed hai and false hai toh subscribe nhi h
+                isSubscribed: {
+                    $cond: {
+                        if: {$in: [req.user?._id, "$subscribers.subscriber"]},
+                        then: true,
+                        else: false
+                    }
+                }
+            }
+        },
+        {
+            $project:{
+                fullName:1,// means fullName is passed as the flag is on
+                username:1,
+                subscribersCount:1,
+                channelsSubscribedToCount:1,
+                isSubscribed:1,
+                avatar: 1,
+                coverImage: 1,
+                email: 1
+
+
+
+            }
+        }
+    ])
+    if (!channel?.length) {
+        throw new ApiError(404, "channel does not exists")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, channel[0], "User channel fetched successfully")
+    )
+
+})
+
 
 
 
@@ -490,7 +567,8 @@ export {
     updateUserAvatar,
     updateUserCoverImage,
     deleteOldCoverImage,
-    deleteUserAvatar
+    deleteUserAvatar,
+    getUserChannelProfile
 
     
 
