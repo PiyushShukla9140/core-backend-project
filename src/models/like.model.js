23c9b67e97cc68f33e89deb2b1b1d92a1ef11b07
Piyush Shukla <piyushshukla8980@gmail.com>
2026-06-26 11:31:26 +0530

    feat: add models for comments, likes, playlists, and tweets with pagination support

---
+++ src/models/like.model.js
@@ -0,0 +1,25 @@
+import mongoose, { SchemaTypes } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2"
+
+const likeSchema = new mongoose.Schema({
+    comment:{
+        type:Schema.Types.ObjectId,
+        ref:"comment"
+    },
+    video:{
+        type:Schema.Types.ObjectId,
+        ref:"video"
+    },
+    likedBy:{
+        type:Schema.Types.ObjectId,
+        ref:"User"
+    },
+    tweet:{
+        type:Schema.Types.ObjectId,
+        ref:"tweets"
+    }
+},{timestamps:true})
+
+likeSchema.plugin(mongooseAggregatePaginate)
+
+export const Like = mongoose.model("Like",likeSchema)
